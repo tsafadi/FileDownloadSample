c76e817c845d3c785e252954a124999b6cd249f3
Tom el Safadi <tsafadi@kes-soft.de>
2019-11-07 18:09:07 -0600

    Initial Commit

---
+++ FileDownloadSample/Controllers/DownloadController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileDownloadSample.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class DownloadController : ControllerBase
+    {
+        private IHostingEnvironment _hostingEnvironment;
+        public DownloadController(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            // Since this is just and example, I am using a local file located inside wwwroot
+            // Afterwards file is converted into a stream
+            var path = Path.Combine(_hostingEnvironment.WebRootPath, "Sample.xlsx");
+            var fs = new FileStream(path, FileMode.Open);
+
+            // Return the file. A byte array can also be used instead of a stream
+            return File(fs, "application/ocet-stream", "Sample.xlsx");
+        }
+    }
+}
